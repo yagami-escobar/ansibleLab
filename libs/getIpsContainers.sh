bf6be51238f65cb6c3b692f4a9487e7230a5c167
yagamiSoaint <yagami19972024@hotmail.com>
2025-07-15 16:41:22 -0500

    Sync

--- libs/getIpsContainers.sh
+++ libs/getIpsContainers.sh
@@ -2,5 +2,5 @@
 
 echo "----------------------------------- REPORT IPS [CONTAINERS] -----------------------------------"
 
-docker network inspect ansiblelab_default \
+docker network inspect ansiblelab_ansible_net \
 | jq -r '.[] | .Containers | to_entries[] | "\(.value.Name) ->  \(.value.IPv4Address)"'
